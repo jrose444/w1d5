7ad49e89f904a7a0ae33508b82897fd27085368c
Aditya Agarwala <aditya@agarwala.us>
2019-05-17 14:36:32 -0700

    complete phase 1

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(parent)
+    @parent = parent
+    parent.children << self unless parent.children.include?(self)
+  end
+
 
 end
